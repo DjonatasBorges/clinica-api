80f2642f6495fd11320d433b29da8f7b5c3e47a5
Djonatas Borges <interdjonatas@gmail.com>
2021-07-22 19:08:17 -0300

    add

--- clinica/settings/heroku.py
+++ clinica/settings/heroku.py
@@ -1,20 +1,14 @@
 import environ
 
-
 from clinica.settings.base import *
-
 
 env = environ.Env()
 
-
 DEBUG = env.bool("DEBUG", False)
-
 
 SECRET_KEY = env("SECRET_KEY")
 
-
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-
 
 DATABASES = {
     "default": env.db(),
